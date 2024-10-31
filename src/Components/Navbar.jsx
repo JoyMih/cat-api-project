3d18392cbd550c30c7596d4b7b1855c38d49c807
Joy H <joyhwangbvt2023@gmail.com>
2024-10-30 19:41:40 -0700

    Added react dom routing practices to ensure that page refresh can occur when top left link in navbar is clicked, as well as adjusting css for the link in navbar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,13 +1,17 @@
+import { Link } from "react-router-dom";
+
 function Navbar() {
-    const handleRefresh = (event) => {
-        event.preventDefault(); // Default anchor tag link behavior is disabled essentially
-        window.location.reload();
-    }
+    /* old code to refresh page without react dom routing */
+    // const handleRefresh = (event) => {
+    //     event.preventDefault(); // Default anchor tag link behavior is disabled essentially
+    //     window.location.reload();
+    // }
 
     return (
         <div className="nav-bar">
             <nav className="nav-items">
-                <p><a href="./src/index.html" onClick={handleRefresh}>Look at these Silly Cats</a></p>
+                {/* <p><a href="./src/index.html" onClick={handleRefresh}>Look at these Silly Cats</a></p> */}
+                <Link to={window.location.pathname} className="refreshLink">Look at these Silly Cats</Link>
                 <ul className="nav-ul">
                     <li><a href="https://thecatapi.com/" target="_blank">Cat API Info</a></li>
                 </ul>
